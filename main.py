59731efa397878c573bd6b49e06fa4e181488ebd
Alexander Willet <al.willet02@gmail.com>
2023-05-07 14:30:53 -0600

    Demo
    Did a demo showcasing basic python

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-print("Hello, Word")
+"""
+    or: a or b
+    and: a and b
+    equivalence: a == b
+    relational: >, <, <=, >=
+"""
+
+"""
+    FUNCTIONS
+"""
+def mainLoop():
+    """
+        Main loop for codebase
+    """
+    is_running = True
+    count = 0
+    while (is_running == True):
+        count = count + 1
+        if (count == 2):
+            is_running = False
+
+        print("True")
+    pass
+
+"""
+    MAIN
+"""
+if __name__ == "__main__":
+    mainLoop()
+    print("Wow")
+    pass
